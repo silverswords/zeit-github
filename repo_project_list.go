2286daa6a6830234241f9bbf3cbbe78ac935817f
yang <1351599172@qq.com>
2019-07-16 21:49:07 +0800

    add project api

---
+++ repo_project_list.go
@@ -0,0 +1,58 @@
+package github
+
+import (
+	"context"
+	"net/http"
+
+	gogithub "github.com/google/go-github/github"
+	cloudapi "github.com/silverswords/clouds/openapi/github"
+	util "github.com/silverswords/clouds/pkgs/http"
+	cloudpkgs "github.com/silverswords/clouds/pkgs/http/context"
+	"golang.org/x/oauth2"
+)
+
+// RepoProjectList lists the projects for a repo.
+func RepoProjectList(w http.ResponseWriter, r *http.Request) {
+	var (
+		github struct {
+			Owner string `json:"owner" zeit:"required"`
+			Repo  string `json:"repo"  zeit:"required"`
+			State string `json:"state" zeit:"required"`
+		}
+	)
+
+	c := cloudpkgs.NewContext(w, r)
+	err := c.ShouldBind(&github)
+	if err != nil {
+		c.WriteJSON(http.StatusBadRequest, cloudpkgs.H{"status": http.StatusBadRequest})
+		return
+	}
+
+	err = util.Validate(&github)
+	if err != nil {
+		c.WriteJSON(http.StatusPreconditionRequired, cloudpkgs.H{"status": http.StatusPreconditionRequired})
+		return
+	}
+
+	token := c.Request.Header
+	t := token.Get("Authorization")
+
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: t},
+	)
+
+	tc := oauth2.NewClient(ctx, ts)
+	client := cloudapi.NewAPIClient(tc)
+
+	opt := &gogithub.ProjectListOptions{
+		State: github.State,
+	}
+	contributor, _, err := client.Client.Repositories.ListProjects(ctx, github.Owner, github.Repo, opt)
+	if err != nil {
+		c.WriteJSON(http.StatusRequestTimeout, cloudpkgs.H{"status": http.StatusRequestTimeout})
+		return
+	}
+
+	c.WriteJSON(http.StatusOK, cloudpkgs.H{"status": http.StatusOK, "contributors": contributor})
+}
