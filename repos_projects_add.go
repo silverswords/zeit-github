fd52eeaf7116c86f5fc91f59c794d9d042874567
yang <1351599172@qq.com>
2019-07-17 10:27:51 +0800

    update file name

---
+++ repos_projects_add.go
@@ -0,0 +1,66 @@
+package github
+
+import (
+	"context"
+	"net/http"
+
+	gogithub "github.com/google/go-github/github"
+	cloudapi "github.com/silverswords/clouds/openapi/github"
+	util "github.com/silverswords/clouds/pkgs/http"
+	cloudpkgs "github.com/silverswords/clouds/pkgs/http/context"
+	"golang.org/x/oauth2"
+)
+
+// RepoProjectAdd creates a GitHub Project for the specified repository.
+func RepoProjectAdd(w http.ResponseWriter, r *http.Request) {
+	var (
+		github struct {
+			Owner                  string `json:"owner"  zeit:"required"`
+			Repo                   string `json:"repo"   zeit:"required"`
+			Name                   string `json:"name"   zeit:"required"`
+			Body                   string `json:"body"   zeit:"required"`
+			State                  string `json:"state"`
+			Public                 bool   `json:"pulic"`
+			OrganizationPermission string `json:"organizationPermission"`
+		}
+	)
+
+	c := cloudpkgs.NewContext(w, r)
+	err := c.ShouldBind(&github)
+	if err != nil {
+		c.WriteJSON(http.StatusBadRequest, cloudpkgs.H{"status": http.StatusBadRequest})
+		return
+	}
+
+	err = util.Validate(&github)
+	if err != nil {
+		c.WriteJSON(http.StatusPreconditionRequired, cloudpkgs.H{"status": http.StatusPreconditionRequired})
+		return
+	}
+
+	token := c.Request.Header
+	t := token.Get("Authorization")
+
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: t},
+	)
+
+	tc := oauth2.NewClient(ctx, ts)
+	client := cloudapi.NewAPIClient(tc)
+
+	opt := &gogithub.ProjectOptions{
+		Name:                   &github.Name,
+		Body:                   &github.Body,
+		State:                  &github.State,
+		OrganizationPermission: &github.OrganizationPermission,
+		Public:                 &github.Public,
+	}
+	contributor, _, err := client.Client.Repositories.CreateProject(ctx, github.Owner, github.Repo, opt)
+	if err != nil {
+		c.WriteJSON(http.StatusRequestTimeout, cloudpkgs.H{"status": http.StatusRequestTimeout})
+		return
+	}
+
+	c.WriteJSON(http.StatusOK, cloudpkgs.H{"status": http.StatusOK, "contributors": contributor})
+}
---
+++ users_orgs.go
@@ -0,0 +1,61 @@
+package github
+
+import (
+	"context"
+	"net/http"
+
+	gogithub "github.com/google/go-github/github"
+	cloudapi "github.com/silverswords/clouds/openapi/github"
+	util "github.com/silverswords/clouds/pkgs/http"
+	cloudpkgs "github.com/silverswords/clouds/pkgs/http/context"
+	"golang.org/x/oauth2"
+)
+
+// UserOrgs lists the organizations for a user. Passing the empty string will list
+// organizations for the authenticated user.
+func UserOrgs(w http.ResponseWriter, r *http.Request) {
+	var (
+		github struct {
+			User    string `json:"user"     zeit:"required"`
+			Page    int    `json:"page"     zeit:"required"`
+			PerPage int    `json:"per_page" zeit:"required"`
+		}
+	)
+
+	c := cloudpkgs.NewContext(w, r)
+	err := c.ShouldBind(&github)
+	if err != nil {
+		c.WriteJSON(http.StatusBadRequest, cloudpkgs.H{"status": http.StatusBadRequest})
+		return
+	}
+
+	err = util.Validate(&github)
+	if err != nil {
+		c.WriteJSON(http.StatusPreconditionRequired, cloudpkgs.H{"status": http.StatusPreconditionRequired})
+		return
+	}
+
+	token := c.Request.Header
+	t := token.Get("Authorization")
+
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: t},
+	)
+
+	tc := oauth2.NewClient(ctx, ts)
+	client := cloudapi.NewAPIClient(tc)
+
+	opt := &gogithub.ListOptions{
+		Page:    github.Page,
+		PerPage: github.PerPage,
+	}
+
+	org, _, err := client.Client.Organizations.List(ctx, github.User, opt)
+	if err != nil {
+		c.WriteJSON(http.StatusRequestTimeout, cloudpkgs.H{"status": http.StatusRequestTimeout})
+		return
+	}
+
+	c.WriteJSON(http.StatusOK, cloudpkgs.H{"status": http.StatusOK, "orgs": org})
+}
